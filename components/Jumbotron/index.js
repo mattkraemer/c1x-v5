2b3a788b449b12d403abad8954499b36daa0ed1f
malmosands <matthias@c1x.dev>
2021-07-22 10:45:40 +0200

    update

--- components/Jumbotron/index.js
+++ components/Jumbotron/index.js
@@ -5,13 +5,15 @@ export default function Jumbotron() {
   return (
     <section className={styles.jumbotron}>
       <div className={styles.inner}>
-        <div className="container">
+        <div className={styles.left}>
           <h1>Modern Frontend Development</h1>
           <h2>I build state-of-the-art User Interfaces, Websites & Web Applications</h2>
           <p>Check out my current projects or learn more about me.</p>
         </div>
+        <div className={styles.right}>
+          <Jumbo class={styles.jumbo} />
+        </div>
       </div>
-      <Jumbo class={styles.jumbo} />
     </section>
   );
 };
