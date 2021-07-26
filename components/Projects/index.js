34834bd0c1fc0a830fa511ff3fdfba5a44f961a0
malmosands <matthias@c1x.dev>
2021-07-26 19:02:20 +0200

    update

--- components/Projects/index.js
+++ components/Projects/index.js
@@ -1,12 +1,45 @@
 import styles from '../../styles/Projects.module.scss'
+import ProjectsJSON from '../../json/projects.json'
+import Link from'next/link'
 
 export default function Projects() {
   return (
-    <section className={styles.jumbotron}>
+    <section className={styles.projects}>
       <div className="container">
-        <h1>Projects</h1>
-        <h2>I build state-of-the-art User Interfaces, Websites & Web Applications</h2>
-        <p>Check out my current projects or learn more about me.</p>
+        <header className={styles.sectionHeader}>
+          <h1>Projects</h1>
+        </header>
+        <div className={styles.wrapper}>
+          <div className={styles.grid}>
+            {
+              ProjectsJSON.map((item, index) => (
+                <Link href={item.url}>
+                  <a key={index} className={styles.project}>
+                    <header className={styles.projectHeader}>
+                      <span>{item.year}</span>
+                      <h1>{item.title}</h1>
+                    </header>
+                    <section className={styles.projectContent}>
+                      <h2>Description</h2>
+                      <p>{item.description}</p>
+                    </section>
+                    <footer className={styles.projectFooter}>
+                      <h2>Techstack</h2>
+                      <ul>
+                        {
+                          item.tech.map(tech => (
+                            <li>{tech}</li>
+                          ))
+                        }
+                      </ul>
+                    </footer>
+
+                  </a>
+                </Link>
+              ))
+            }
+          </div>
+        </div>
       </div>
     </section>
   );
--- components/Services/index.js
+++ components/Services/index.js
@@ -7,31 +7,33 @@ export default function Services() {
         <header>
           <h1>Services</h1>
         </header>
-        <div className={styles.grid}>
-          <article>
-            <h1>Frontend Development</h1>
-            <p>Turning designs into usable, production-ready and future-proof User Interfaces.</p>
-          </article>
-          <article>
-            <h1>Web Applications</h1>
-            <p>Creating Applications for the web with features like User-Authentication, Payments & more.</p>
-          </article>
-          <article>
-            <h1>Minimum Viable Products (MVP)</h1>
-            <p>Building fully working MVPs with all the features needed in a short time.</p>
-          </article>
-          <article>
-            <h1>Websites</h1>
-            <p>Building & designing modern Websites with a variety of tools for clients of all kind.</p>
-          </article>
-          <article>
-            <h1>Optimization & Refactoring</h1>
-            <p>Optimizing existing Websites & Applications for Performance, Accessibility and more.</p>
-          </article>
-          <article>
-            <h1>Prototypes & Mockups</h1>
-            <p>Turning ideas into Mockups or Prototypes for presentations and more.</p>
-          </article>
+        <div className={styles.wrapper}>
+          <div className={styles.grid}>
+            <article>
+              <h1>Frontend Development</h1>
+              <p>Turning designs into usable, production-ready and future-proof User Interfaces.</p>
+            </article>
+            <article>
+              <h1>Web Applications</h1>
+              <p>Creating Applications for the web with features like User-Authentication, Payments & more.</p>
+            </article>
+            <article>
+              <h1>Minimum Viable Products (MVP)</h1>
+              <p>Building fully working MVPs with all the features needed in a short time.</p>
+            </article>
+            <article>
+              <h1>Websites</h1>
+              <p>Building & designing modern Websites with a variety of tools for clients of all kind.</p>
+            </article>
+            <article>
+              <h1>Optimization & Refactoring</h1>
+              <p>Optimizing existing Websites & Applications for Performance, Accessibility and more.</p>
+            </article>
+            <article>
+              <h1>Prototypes & Mockups</h1>
+              <p>Turning ideas into Mockups or Prototypes for presentations and more.</p>
+            </article>
+          </div>
         </div>
       </div>
     </section>
