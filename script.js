102301d804f77854e67e93d5116623976c85134d
malmosands <matthias@c1x.dev>
2021-10-14 18:16:01 +0200

    update

--- script.js
+++
@@ -1,8 +0,0 @@
-let circle1 = document.getElementById('circle1');
-
-const onMouseMove = (e) =>{
-  circle1.style.left = e.pageX + 'px';
-  circle1.style.top = e.pageY + 'px';
-}
-
-document.addEventListener('mousemove', onMouseMove);
