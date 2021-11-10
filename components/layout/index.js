c08c79f29aa15db7463cc8729c72a2a79b6ea6db
malmosands <matthias@c1x.dev>
2021-11-10 14:59:09 +0100

    layout update

--- components/layout/index.js
+++ components/layout/index.js
@@ -5,17 +5,18 @@ import Navbar from './navbar'
 export default function Layout({ children }) {
 
   useEffect(() => {
-    let circle1 = document.getElementById('circle1');
-
-    const onMouseMove = (e) =>{
+    let circle1 = document.querySelector('#circle1');
+    document.addEventListener('mousemove', (e) => {
       circle1.style.left = e.pageX + 'px';
       circle1.style.top = e.pageY + 'px';
-    }
+    });
 
-    document.addEventListener('mousemove', onMouseMove);
+    let circle2 = document.querySelector('#circle2');
+    document.addEventListener('mousemove', (e) => {
+      circle2.style.left = e.pageX + 'px';
+    });
 
     let tronGridElem = document.querySelector('#tronGrid');
-
     document.addEventListener('scroll', () => {
       tronGridElem.style.bottom = '-' + (window.pageYOffset / 200) + '%';
     })
@@ -25,6 +26,7 @@ export default function Layout({ children }) {
   return (
     <div class={styles.layout}>
       <div id="circle1" class={styles.circle1}></div>
+      <div id="circle2" class={styles.circle2}></div>
       <div id="tronGrid" class={styles.bgGrid}></div>
       <Navbar />
       <main class={styles.main}>
