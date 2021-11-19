48f8f7200678b6c5e462f58c2d354f4b06d12f77
malmosands <matthias@c1x.dev>
2021-11-19 16:03:04 +0100

    update

--- components/Header/index.js
+++ components/Header/index.js
@@ -8,9 +8,11 @@ export default function Header() {
       <div class="container">
         <div class={styles.inner}>
           <h1>I build state-of-the-art User Interfaces, Websites & Web Applications</h1>
-          <p>
-            Check out my <Link href="#projects"><a>current projects</a></Link> or <Link href="#about"><a>learn more about me</a></Link>.
-          </p>
+          <div>
+            <p>
+              Check out my <Link href="#projects"><a>current projects</a></Link> or <Link href="#about"><a>learn more about me</a></Link>.
+            </p>
+          </div>
         </div>
       </div>
     </div>
