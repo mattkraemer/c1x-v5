76c8aee938489fb55016f9e3ba5e224a290d766c
malmosands <matthias@c1x.dev>
2021-12-03 17:54:46 +0100

    project screenshots quickfix

--- next.config.js
+++ next.config.js
@@ -1,10 +1,3 @@
 module.exports = {
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"]
-    });
-
-    return config;
-  }
-};
+  reactStrictMode: true,
+}
