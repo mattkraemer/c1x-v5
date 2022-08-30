460af6250e8ee2105dcfdfa5e8eedd6f0f561513
malmosands <matthias@c1x.dev>
2022-08-30 22:19:24 +0200

    Fix: links fixed and target added

--- components/layout/navbar.js
+++ components/layout/navbar.js
@@ -21,15 +21,15 @@ export default function Navbar() {
         </div>
         <a href="" class={styles.logo}><span class={styles.c1x}>c1x</span><span class={styles.dev}>dev</span></a>
         <div class={styles.navSection}>
-          <Link href="https://stackoverflow.com/users/7437415/matthias-kr%c3%a4mer">
-            <a>
+          <Link href="https://github.com/mattkraemer">
+            <a target="_blank">
               <svg viewBox="0 0 140 140" xmlns="http://www.w3.org/2000/svg">
                 <path d="M70 2.917a67.083 67.083 0 0 0-21.204 130.748c3.348.583 4.41-1.382 4.41-3.156 0-1.604.035-6.049 0-11.666-18.667 4.048-22.523-8.838-22.523-8.838a17.75 17.75 0 0 0-7.443-9.812c-6.09-4.165.455-4.083.455-4.083a14.082 14.082 0 0 1 10.278 6.907 14.28 14.28 0 0 0 19.519 5.576 14.292 14.292 0 0 1 4.258-8.936c-14.892-1.704-30.555-7.455-30.555-33.169a25.94 25.94 0 0 1 6.913-18.001 24.068 24.068 0 0 1 .653-17.751s5.64-1.803 18.445 6.883a63.484 63.484 0 0 1 33.6 0c12.833-8.68 18.427-6.883 18.427-6.883a24.098 24.098 0 0 1 .665 17.75A25.912 25.912 0 0 1 112.8 66.5c0 25.783-15.686 31.447-30.63 33.098a15.907 15.907 0 0 1 4.59 12.367v18.573c0 1.791 1.085 3.774 4.492 3.127A67.083 67.083 0 0 0 70 2.917Z" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="5.83333"/>
               </svg>
             </a>
           </Link>
           <Link href="https://www.linkedin.com/in/matthias-kraemer1">
-            <a>
+            <a target="_blank">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 140 140">
                 <g transform="scale(5.83333)">
                   <path d="M6.5 22.5h-5v-13h5Zm9-9a2 2 0 0 0-2 2v7h-5v-13h5v1.485a6.307 6.307 0 0 1 3.99-1.495c2.962 0 5.01 2.2 5.01 6.355V22.5h-5v-7a2 2 0 0 0-2-2ZM6.5 5A2.5 2.5 0 1 1 4 2.5 2.5 2.5 0 0 1 6.5 5Z" fill="none" stroke-linecap="round" stroke-linejoin="round"/>
@@ -37,8 +37,8 @@ export default function Navbar() {
               </svg>
             </a>
           </Link>
-          <Link href="https://github.com/mattkraemer">
-            <a>
+          <Link href="https://stackoverflow.com/users/7437415/matthias-kr%c3%a4mer">
+            <a target="_blank">
               <svg viewBox="0 0 140 140" xmlns="http://www.w3.org/2000/svg">
                 <g fill="none" stroke-linecap="round" stroke-linejoin="round">
                   <path d="M102.083 87.5v37.917a11.667 11.667 0 0 1-11.666 11.666H26.25a11.667 11.667 0 0 1-11.667-11.666V87.5M81.667 113.75H35M83.924 98.082l-45.657-9.69M89.378 83.213 46.731 64.266M97.807 69.808 60.02 42.425M108.827 58.438 77.542 23.812M121.97 49.607 98.547 9.24" stroke-width="5.83333"/>
